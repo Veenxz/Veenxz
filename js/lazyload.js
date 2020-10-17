2651234731d8a1d4a15140ae74f674a407f843a6
Veenxz-win <42843131+Veenxz@users.noreply.github.com>
2020-10-17 18:28:38 +0800

    Site updated: 2020-10-17 18:28:37

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,49 +1,58 @@
-(function (window) {
-	var images = Array.prototype.slice.call(document.querySelectorAll('img[srcset]'));
+// eslint-disable-next-line no-unused-expressions
+!(function(window, document) {
+  var images = Array.prototype.slice.call(document.querySelectorAll('img[srcset]'));
 
-	function elementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-		var height = window.innerHeight || document.documentElement.clientHeight;
-		return (
-			rect.top >= 0
-			&& rect.left >= 0
-			&& rect.top <= height * 1.5
-		);
-	}
-	function loadImage(el, fn) {
-		var img = new Image(), src = el.getAttribute('src');
-		img.onload = function () {
-			el.srcset = src;
-			fn ? fn() : null;
-		};
-		img.srcset = src;
-	}
+  function elementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    var height = window.innerHeight || document.documentElement.clientHeight;
+    return (
+      rect.top >= 0
+      && rect.left >= 0
+      && rect.top <= height * 3
+    );
+  }
 
-	function processImages() {
-		for (var i = 0; i < images.length; i++) {
-			if (elementInViewport(images[i])) {
-				(function(index){
-					var loadingImage = images[index];
-					loadImage(loadingImage, function () {
-						images = images.filter(function(t) {
-							return loadingImage !== t;
-						});
-					});
-				})(i);
-			}
-		}
-	}
+  function loadImage(el, fn) {
+    var img = new Image();
+    var src = el.getAttribute('src');
+    img.onload = function() {
+      el.srcset = src;
+      fn && fn();
+    };
+    img.srcset = src;
+  }
 
-	function throttle(method, context) {
-		clearTimeout(method.tId);
-		method.tId = setTimeout(function () {
-				method.call(context);
-		}, 200);
-	}
+  function processImages() {
+    for (var i = 0; i < images.length; i++) {
+      if (elementInViewport(images[i])) {
+        // eslint-disable-next-line no-loop-func
+        (function(index) {
+          var loadingImage = images[index];
+          loadImage(loadingImage, function() {
+            images = images.filter(function(t) {
+              return loadingImage !== t;
+            });
+          });
+        })(i);
+      }
+    }
+    if (images.length === 0) {
+      window.removeEventListener('scroll', imageLazyLoader);
+    }
+  }
 
-	processImages();
+  function throttle(method, context) {
+    clearTimeout(method.tId);
+    method.tId = setTimeout(function() {
+      method.call(context);
+    }, 100);
+  }
 
-	window.addEventListener('scroll', function () {
-		throttle(processImages, window);
-	});
-})(this);
+  var imageLazyLoader = function() {
+    throttle(processImages, window);
+  };
+
+  processImages();
+
+  window.addEventListener('scroll', imageLazyLoader);
+})(window, document);
